dde3b42f0273186c6dc654f64a62978f16c64316
Jacob Flory <jakea.flory@gmail.com>
2025-07-29 21:40:49 -0400

    Created functions for logging all children of arrays

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 
 
-const knightsCode [
-    To fear god and maintain his church
-]
-
-const knightsCodeBurgandy [
-    Faith,
-    Charity,
-    Justice,
-    Sagacity,
-    Prudence,
-    Temperance,
-    Resolution,
-    Truth,
-    Liberality,
-    Diligence,
-    Hope,
-    Valor,
+const knightsCode = [
+    'To fear god and maintain his church'
 ];
 
-knightsCode.forEach(knight => console.log(knight));
-knightsCodeBurgandy.forEach(knightB => console.log(knightB));
+const knightsCodeBurgandy = [
+    'Faith',
+    'Charity',
+    'Justice',
+    'Sagacity',
+    'Prudence',
+    'Temperance',
+    'Resolution',
+    'Truth',
+    'Liberality',
+    'Diligence',
+    'Hope',
+    'Valor'
+];
 
+function runKnight() {
+    knightsCode.forEach(knight => console.log(knight))
+};
+function runKnightB() {
+    knightsCodeBurgandy.forEach(knightB => console.log(knightB));
+};
+
+runKnight();
+runKnightB();
