c26037d19a8ebdf31b86fdef3d822c52961bd66d
Jacob Flory <jakea.flory@gmail.com>
2025-07-29 20:54:15 -0400

    Created initial database
    Created initial database for Knight's Code

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+const knightsCode [
+    To fear god and maintain his church
+]
+
+const knightsCodeBurgandy [
+    Faith,
+    Charity,
+    Justice,
+    Sagacity,
+    Prudence,
+    Temperance,
+    Resolution,
+    Truth,
+    Liberality,
+    Diligence,
+    Hope,
+    Valor,
+];
+
+knightsCode.forEach(knight => console.log(knight));
+knightsCodeBurgandy.forEach(knightB => console.log(knightB));
+
