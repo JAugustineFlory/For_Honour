bfb9d0c842356566a5962ec19fda0d06467035a6
Jacob Flory <jakea.flory@gmail.com>
2025-07-31 11:53:31 -0400

    Updates READ.me to include sources for Codes; Adds explaination text to script.js

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-
+//Collections of Codes
 honours = [
-    knightsCode = [
-        'To fear God and maintain His Church',
-        'To serve the liege lord in valour and faith',
-        'To protect the weak and defenceless',
-        'To give succour to widows and orphans',
-        'To refrain from the wanton giving of offence',
-        'To live by honour and for glory',
-        'To despise pecuniary reward',
-        'To fight for the welfare of all',
-        'To obey those placed in authority',
-        'To guard the honour of fellow knights',
-        'To eschew unfairness, meanness and deceit',
-        'To keep faith',
-        'At all times to speak the truth',
-        'To persevere to the end in any enterprise begun',
-        'To respect the honour of women',
-        'Never to refuse a challenge from an equal',
-        'Never to turn the back upon a foe'
-    ],
-    knightsCodeBurgandy = [
-        'Faith',
-        'Charity',
-        'Justice',
-        'Sagacity',
-        'Prudence',
-        'Temperance',
-        'Resolution',
-        'Truth',
-        'Liberality',
-        'Diligence',
-        'Hope',
-        'Valor'
+    // Knight's collection
+    knights = [
+        //Code of Chivalry translated from The Song of Roland 
+        knightsCode = [
+            'To fear God and maintain His Church',
+            'To serve the liege lord in valour and faith',
+            'To protect the weak and defenceless',
+            'To give succour to widows and orphans',
+            'To refrain from the wanton giving of offence',
+            'To live by honour and for glory',
+            'To despise pecuniary reward',
+            'To fight for the welfare of all',
+            'To obey those placed in authority',
+            'To guard the honour of fellow knights',
+            'To eschew unfairness, meanness and deceit',
+            'To keep faith',
+            'At all times to speak the truth',
+            'To persevere to the end in any enterprise begun',
+            'To respect the honour of women',
+            'Never to refuse a challenge from an equal',
+            'Never to turn the back upon a foe'
+        ],
+        knightsCodeBurgandy = [
+            'Faith',
+            'Charity',
+            'Justice',
+            'Sagacity',
+            'Prudence',
+            'Temperance',
+            'Resolution',
+            'Truth',
+            'Liberality',
+            'Diligence',
+            'Hope',
+            'Valor'
+        ]
     ]
 ];
 
